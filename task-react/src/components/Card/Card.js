2b2c9192ef5c072ddf3658bf7a562669efe1096b
JQiue <861947542@qq.com>
2022-03-21 20:09:29 +0800

    better style

--- task-react/src/components/Card/Card.js
+++ task-react/src/components/Card/Card.js
@@ -1,10 +1,13 @@
 import './card.css';
-import { useState, useEffect, } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function InputComponent(props) {
   const addHandle = (e) => {
-    console.log(e.type);
     if (e.code == 'Enter' || e.type == 'blur') {
+      if (e.target.value == '') {
+        props.toggle();
+        return;
+      }
       let data = JSON.stringify({ content: e.target.value, type: 'prepare' })
       global.http.post('/api/task/new', data, result => {
         global.http.get('/api/task/list', result => props.setData(result.data));
@@ -13,12 +16,14 @@ function InputComponent(props) {
       props.toggle();
     }
   }
-  return (<input className='input-component' type="input" placeholder='请输点什么...' onBlur={addHandle} onKeyDown={addHandle} />)
+  return (<input className='input-component' type="input" placeholder='请输点什么...' autoFocus onBlur={addHandle} onKeyDown={addHandle} />)
 }
 
 export default function Card(props) {
   let [data, setData] = useState([]);
   let [show, setShow] = useState(false);
+  let learningContainer = useRef(null);
+  let completeContainer = useRef(null);
 
   useEffect(() => {
     global.http.get('/api/task/list', result => setData(data => result.data));
@@ -36,19 +41,19 @@ export default function Card(props) {
     global.http.post('/api/task/update',
       data,
       result => {
-        console.log(result);
         if (result.error == 0) global.http.get('/api/task/list', result => setData(result.data));
       }
     );
+    learningContainer.current.classList.remove('dragover');
   };
 
   const completedDropHandler = (e) => {
     const id = e.dataTransfer.getData('text/plain');
     const data = JSON.stringify({ id, type: 'completed' });
     global.http.post('/api/task/update', data, result => {
-      console.log(result);
       global.http.get('/api/task/list', result => setData(result.data));
     });
+    completeContainer.current.classList.remove('dragover');
   };
 
   const deleteItem = (id) => {
@@ -56,6 +61,23 @@ export default function Card(props) {
     global.http.post('/api/task/delete', data, result => {
       global.http.get('/api/task/list', result => setData(result.data));
     });
+  };
+
+  const learnDragOverHandler = e => {
+    e.preventDefault();
+    learningContainer.current.classList.add('dragover');
+  }
+  const learnDragLeaveHandler = e => {
+    learningContainer.current.classList.remove('dragover');
+  }
+
+  const completeDragOverHandler = e => {
+    e.preventDefault();
+    completeContainer.current.classList.add('dragover');
+  }
+
+  const completeDragLeaveHandler = e => {
+    completeContainer.current.classList.remove('dragover');
   }
 
   return (<div className="wrapper">
@@ -68,39 +90,48 @@ export default function Card(props) {
             return (
               <div className='draggable' draggable="true"
                 onDragStart={dragStartHandle.bind(this, item.id)} key={item.id}>{item.content}
-                <button onClick={deleteItem.bind(this, item.id)}>删除</button>
+                <button className='delete-btn' onClick={deleteItem.bind(this, item.id)}>x</button>
               </div>
             )
           }
         })
       }
-      <div>
+      <div className='draggable-input'>
         {show ? <InputComponent toggle={toggle} setData={setData} /> : null}
       </div>
       <div style={{ textAlign: 'center' }}>
-        <button onClick={() => toggle()}>+</button>
+        <button className='add-btn' onClick={() => toggle()}>+</button>
       </div>
     </div>
 
-    <div className="card" onDragOver={(e) => e.preventDefault()} onDrop={learningDropHandler}>
+    <div className="card"
+      ref={learningContainer}
+      onDragLeave={learnDragLeaveHandler}
+      onDragOver={learnDragOverHandler}
+      onDrop={learningDropHandler}>
       <h3>学习中</h3>
       {
         data.map((item, index) => {
-          if (item.type == 'learning') return (<div className='draggable' draggable="true" onDragStart={dragStartHandle.bind(this, item.id)} key={item.id}>
+          if (item.type == 'learning') return (<div className='draggable' draggable="true" style={{backgroundColor: 'green', color: 'white'}} onDragStart={dragStartHandle.bind(this, item.id)} key={item.id}>
             {item.content}
-            <button onClick={deleteItem.bind(this, item.id)}>删除</button>
+            <button className='delete-btn' onClick={deleteItem.bind(this, item.id)}>x</button>
           </div>)
         })
       }
     </div>
 
-    <div className="card" onDragOver={(e) => e.preventDefault()} onDrop={completedDropHandler}>
+    <div className="card" 
+    ref={completeContainer}
+    onDragOver={completeDragOverHandler}
+    onDrop={completedDropHandler}
+    onDragLeave={completeDragLeaveHandler}
+    >
       <h3>完成</h3>
       {
         data.map((item, index) => {
-          if (item.type == 'completed') return (<div className="draggable" draggable="true" onDragStart={dragStartHandle.bind(this, item.id)} key={item.id}>
+          if (item.type == 'completed') return (<div className="draggable" draggable="true" style={{backgroundColor: 'gray', color: 'white'}} onDragStart={dragStartHandle.bind(this, item.id)} key={item.id}>
             {item.content}
-            <button onClick={deleteItem.bind(this, item.id)}>删除</button>
+            <button className='delete-btn' onClick={deleteItem.bind(this, item.id)}>x</button>
           </div>)
         })
       }
--- task-react/src/components/Login/Login.js
+++ task-react/src/components/Login/Login.js
@@ -1,9 +1,33 @@
 import { useNavigate } from 'react-router-dom';
+import './login.css'
 
 export default function Login(props) {
   const navigate = useNavigate();
-  return (<div>
-    <h2>Login Form</h2>
-    <button onClick={() => navigate('/task')}>Go</button>
-  </div>)
+  return (
+    <div className='login'>
+      <div className='weather'>
+        <div className='weather-item'>
+          <span className="city">城市</span>
+          <span className='temp'>温度°</span>
+          <img src="/asserts/qing.png" alt="" />
+        </div>
+        <div className='weather-item'>
+          <span className="city">城市</span>
+          <span className='temp'>温度°</span>
+          <img src="/asserts/yin.png" alt="" />
+        </div>
+        <div className='weather-item'>
+          <span className="city">城市</span>
+          <span className='temp'>温度°</span>
+          <img src="/asserts/yu.png" alt="" />
+        </div>
+        <div className='weather-item'>
+          <span className="city">城市</span>
+          <span className='temp'>温度°</span>
+          <img src="/asserts/yu.png" alt="" />
+        </div>
+      </div>
+      <button className='btn' onClick={() => navigate('/task')}>Go</button>
+    </div>
+  )
 }
